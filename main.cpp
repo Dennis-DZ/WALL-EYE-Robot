d06819255fed8a378ca5f0854739e520a8282ccf
Dennis Zhitenev <denniszhitenev@gmail.com>
2023-03-01 23:08:06 -0500

    Code that completed checkpoint one + PID stuff (untested)

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 DigitalEncoder rightEncoder(FEHIO::P0_0);
 DigitalEncoder leftEncoder(FEHIO::P0_7);
 const int countsPer6Inch = 242;
+const double distancePerCount = 6.0/242;
 const double adjustmentFactor = 1.03;
 const double distanceBetweenWheels = 7.25;
 const double rightMotorComp = 1.02;
@@ -29,6 +30,55 @@ void driveForward(int percent, double distance) {
     leftMotor.SetPercent(percent);
 
     while ((leftEncoder.Counts() + rightEncoder.Counts()) / 2.0 < counts);
+
+    rightMotor.Stop();
+    leftMotor.Stop();
+
+    std::string message = "Drove " + std::to_string(distance) + " in";
+    LCD.WriteLine(message.c_str());
+}
+
+void driveEqual(int percent, double distance) {
+
+    double counts = (distance * countsPer6Inch) / 6 * adjustmentFactor;
+
+    rightEncoder.ResetCounts();
+    leftEncoder.ResetCounts();
+
+    rightMotor.SetPercent(percent);
+    leftMotor.SetPercent(percent);
+
+    while ((leftEncoder.Counts() + rightEncoder.Counts()) / 2.0 < counts);
+
+    rightMotor.Stop();
+    leftMotor.Stop();
+
+    std::string message = "Drove " + std::to_string(distance) + " in";
+    LCD.WriteLine(message.c_str());
+}
+
+void driveUntilStopped(int percent, double distance) {
+
+    double counts = (distance * countsPer6Inch) / 6 * adjustmentFactor;
+
+    rightEncoder.ResetCounts();
+    leftEncoder.ResetCounts();
+
+    rightMotor.SetPercent(percent * rightMotorComp);
+    leftMotor.SetPercent(percent);
+
+    float t = TimeNow();
+    int oldCounts;
+
+    while ((leftEncoder.Counts() + rightEncoder.Counts()) / 2.0 < counts) {
+        if (TimeNow() > 0.5 + t) {
+            t = TimeNow();
+            if (leftEncoder.Counts() - oldCounts < 10) {
+                break;
+            }
+            oldCounts = leftEncoder.Counts();
+        }
+    }
 
     rightMotor.Stop();
     leftMotor.Stop();
@@ -99,7 +149,13 @@ int main() {
     turn(motorPercent, 4);
     Sleep(sleepTime);
 
-    driveForward(motorPercent, 35);
+    driveForward(motorPercent, 11);
+    Sleep(sleepTime);
+
+    driveEqual(motorPercent, 21);
+    Sleep(sleepTime);
+
+    driveForward(motorPercent, 3);
     Sleep(sleepTime);
 
     turn(motorPercent, -94);
@@ -111,16 +167,16 @@ int main() {
     turn(motorPercent, 90);
     Sleep(sleepTime);
 
-    driveForward(motorPercent, 18);
+    driveUntilStopped(motorPercent, 19.5);
     Sleep(sleepTime);
 
-    backUp(motorPercent, 18);
+    backUp(motorPercent, 19);
     Sleep(sleepTime);
 
     turn(motorPercent, -90);
     Sleep(sleepTime);
 
-    backUp(motorPercent, 10);
+    backUp(motorPercent, 13);
     Sleep(sleepTime);
 
     turn(motorPercent, 94);
@@ -130,4 +186,46 @@ int main() {
     Sleep(sleepTime);
 
     return 0;
+}
+
+double rightErrorSum = 0, leftErrorSum = 0, lastRightError = 0, lastLeftError = 0;
+double rightPower = 0, leftPower = 0;
+const double P = 0.75, D = 0.25, I = 0.05;
+
+void driveForwardPID(double distance, double speed) {
+
+    const int sleepTime = 0.15;
+
+    double counts = distance * (1 / distancePerCount) * adjustmentFactor;
+    double t = TimeNow();
+    int lastRightCounts = 0, lastLeftCounts = 0;
+    rightEncoder.ResetCounts();
+    leftEncoder.ResetCounts();
+    
+    Sleep(sleepTime);
+
+    while ((leftEncoder.Counts() + rightEncoder.Counts()) / 2.0 < counts) {
+        rightMotor.SetPercent(rightPIDAdjustment(rightEncoder.Counts() - lastRightCounts, TimeNow() - t, speed));
+        leftMotor.SetPercent(leftPIDAdjustment(leftEncoder.Counts() - lastLeftCounts, TimeNow() - t, speed));
+        t = TimeNow();
+        lastRightCounts = rightEncoder.Counts();
+        lastLeftCounts = leftEncoder.Counts();
+        Sleep(sleepTime);
+    }
+}
+
+double rightPIDAdjustment(int deltaCounts, double deltaTime, double speed) {
+    double error = speed - (distancePerCount * deltaCounts) / deltaTime;
+    rightErrorSum += error;
+    rightPower = rightPower + (error * P) + (rightErrorSum * I) + ((error - lastRightError) * D);
+    lastRightError = error;
+    return rightPower;
+}
+
+double leftPIDAdjustment(int deltaCounts, double deltaTime, double speed) {
+    double error = speed - (distancePerCount * deltaCounts) / deltaTime;
+    leftErrorSum += error;
+    leftPower = leftPower + (error * P) + (leftErrorSum * I) + ((error - lastLeftError) * D);
+    lastLeftError = error;
+    return leftPower;
 }
